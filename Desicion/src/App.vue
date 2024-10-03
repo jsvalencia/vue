bdd7b36884e16e501e045e02cb6cd6ec69c38cba
Sergio Valencia <sergio.valencia.1522@gmail.com>
2024-10-03 16:58:04 -0500

    first commit

---
+++ Desicion/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hola Mundo</h1>
+    <hr/>
+    <MyCounter :value="25"/>
+
+</template>
+
+<script lang="ts" setup>
+import MyCounter from './components/Counter.vue'
+
+
+</script>
+
---
+++ Desicion/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Hola Mundo</h1>
+    <h3>Contador: {{ contador }}</h3>
+    <h3>Cuadrado: {{ contador*contador }}</h3>
+
+    <div>
+        <button @click="contador++">+1</button>
+        <button @click="contador--">-1</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';// Se importa al declarar la variavle reactiva
+
+
+/*const props = defineProps({
+    //value: Number
+    value: {type:Number, required:true}
+})*/
+
+const props = defineProps<{
+    value:number;
+}>();
+
+const contador =ref(props.value);// es automanticamente retornado y se puede usar 
+
+</script>
+
